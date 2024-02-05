008ba97780aaa4ab08a78abe7f0da7ec9a969aa7
lcdavis13 <3443196+lcdavis13@users.noreply.github.com>
2024-02-05 07:34:59 -1000

    clean up maybe

--- sketch3.js
+++ sketch3.js
@@ -1,15 +1,16 @@
 var sketch3 = function(p) {
   var grid, next;
-  var diagonal_rate = 0.709; // 1/sqrt(2)
+  var diagonal_rate = 0.709; // Weighting for diagonal neighbors (1/sqrt(2))
 
+  // Simulation parameters
   p.parameters = {
-      w: {label: 'Width', min: 32, max: 1024, step: 2, value: 200},
-      h: {label: 'Height', min: 32, max: 1024, step: 2, value: 200},
-      dA: { label: 'Diffusion Rate A', min: 0.8, max: 1.2, step: 0.01, value: 1 },
-      dB: { label: 'Diffusion Rate B', min: 0.4, max: 0.6, step: 0.01, value: 0.5 },
-      feed: { label: 'Feed Rate', min: 0.03, max: 0.08, step: 0.001, value: 0.055 },
-      k: { label: 'Kill Rate', min: 0.05, max: 0.07, step: 0.001, value: 0.062 },
-      dt: { label: 'Time Delta', min: 0.8, max: 1.2, step: 0.01, value: 1.0 }
+    w: {label: 'Width', min: 32, max: 1024, step: 2, value: 200},
+    h: {label: 'Height', min: 32, max: 1024, step: 2, value: 200},
+    dA: { label: 'Diffusion Rate A', min: 0.8, max: 1.2, step: 0.01, value: 1 },
+    dB: { label: 'Diffusion Rate B', min: 0.4, max: 0.6, step: 0.01, value: 0.5 },
+    feed: { label: 'Feed Rate', min: 0.03, max: 0.08, step: 0.001, value: 0.055 },
+    k: { label: 'Kill Rate', min: 0.05, max: 0.07, step: 0.001, value: 0.062 },
+    dt: { label: 'Time Delta', min: 0.8, max: 1.2, step: 0.01, value: 1.0 }
   };
 
   p.setup = function() {
@@ -19,151 +20,92 @@ var sketch3 = function(p) {
   };
 
   function initializeGrid() {
-      grid = [];
-      next = [];
-      for (var x = 0; x < p.parameters.w.value; x++) {
-          grid[x] = [];
-          next[x] = [];
-          for (var y = 0; y < p.parameters.h.value; y++) {
-              grid[x][y] = { a: 1, b: 0 };
-              next[x][y] = { a: 1, b: 0 };
-          }
-      }
+    grid = [];
+    next = [];
+    for (var x = 0; x < p.parameters.w.value; x++) {
+        grid[x] = [];
+        next[x] = [];
+        for (var y = 0; y < p.parameters.h.value; y++) {
+            grid[x][y] = { a: 1, b: 0 };
+            next[x][y] = { a: 1, b: 0 };
+        }
+    }
 
-      for (var i = 150; i < 160; i++) {
-          for (var j = 150; j < 160; j++) {
-              grid[i][j].b = 0.5 + 0.5 * p.random();
-          }
-      }
+    for (var i = 150; i < 160; i++) {
+        for (var j = 150; j < 160; j++) {
+            grid[i][j].b = 0.5 + 0.5 * p.random();
+        }
+    }
   }
 
   p.draw = function() {
       p.background(51);
+      updateGrid();
+      drawGrid();
+      swap();
+  };
 
+  function updateGrid() {
       for (var x = 0; x < p.parameters.w.value; x++) {
           for (var y = 0; y < p.parameters.h.value; y++) {
+              var laplaceAVal = laplace(x, y, 'a');
+              var laplaceBVal = laplace(x, y, 'b');
               var a = grid[x][y].a;
               var b = grid[x][y].b;
-              next[x][y].a = a + p.parameters.dt.value *
-                  ((p.parameters.dA.value * laplaceA(x, y)) -
-                  (a * b * b) +
-                  (p.parameters.feed.value * (1 - a)));
-              next[x][y].b = b + p.parameters.dt.value *
-                  ((p.parameters.dB.value * laplaceB(x, y)) +
-                  (a * b * b) -
-                  ((p.parameters.k.value + p.parameters.feed.value) * b));
 
-              next[x][y].a = p.constrain(next[x][y].a, 0, 1);
-              next[x][y].b = p.constrain(next[x][y].b, 0, 1);
+              next[x][y].a = updateConcentration(a, laplaceAVal, b, 'a');
+              next[x][y].b = updateConcentration(b, laplaceBVal, a, 'b');
           }
       }
+  }
 
+  function updateConcentration(conc, laplaceVal, otherConc, type) {
+      var rate = type === 'a' ? p.parameters.dA.value : p.parameters.dB.value;
+      var reactionTerm = type === 'a' ? -conc * otherConc * otherConc + p.parameters.feed.value * (1 - conc) 
+                                      : conc * otherConc * otherConc - (p.parameters.k.value + p.parameters.feed.value) * conc;
+      var diffusionTerm = rate * laplaceVal;
+      var updatedConc = conc + p.parameters.dt.value * (diffusionTerm + reactionTerm);
+      return p.constrain(updatedConc, 0, 1);
+  }
+
+  function laplace(x, y, type) {
+      var sum = 0;
+      var rate = 0.146; // Direct neighbor rate
+
+      sum += grid[x][y][type] * -1;
+      sum += getNeighborConcentration(x - 1, y, type) * rate;
+      sum += getNeighborConcentration(x + 1, y, type) * rate;
+      sum += getNeighborConcentration(x, y - 1, type) * rate;
+      sum += getNeighborConcentration(x, y + 1, type) * rate;
+      sum += getNeighborConcentration(x - 1, y - 1, type) * rate * diagonal_rate;
+      sum += getNeighborConcentration(x + 1, y - 1, type) * rate * diagonal_rate;
+      sum += getNeighborConcentration(x + 1, y + 1, type) * rate * diagonal_rate;
+      sum += getNeighborConcentration(x - 1, y + 1, type) * rate * diagonal_rate;
+
+      return sum;
+  }
+
+  function getNeighborConcentration(x, y, type) {
+      var nx = (x + p.parameters.w.value) % p.parameters.w.value;
+      var ny = (y + p.parameters.h.value) % p.parameters.h.value;
+      return grid[nx][ny][type];
+  }
+
+  function drawGrid() {
       p.loadPixels();
       for (var x = 0; x < p.parameters.w.value; x++) {
           for (var y = 0; y < p.parameters.h.value; y++) {
               var pix = (x + y * p.parameters.w.value) * 4;
               var a = next[x][y].a;
               var b = next[x][y].b;
-              var c = p.floor((a - b) * 255);
-              c = p.constrain(c, 0, 255);
-              p.pixels[pix + 0] = c;
-              p.pixels[pix + 1] = c;
-              p.pixels[pix + 2] = c;
-              p.pixels[pix + 3] = 255;
+              var colorVal = p.floor((a - b) * 255);
+              colorVal = p.constrain(colorVal, 0, 255);
+              setPixelColor(pix, colorVal);
           }
       }
       p.updatePixels();
-
-      swap();
-  };
-
-  function getxy(x, y) {
-    let nx, ny;
-    if (x < 0)
-    {
-      nx = p.parameters.w.value + x;
-    }
-    else if (x >= p.parameters.w.value)
-    {
-      nx = x - p.parameters.w.value;
-    }
-    else
-    {
-      nx = x;
-    }  
-    if (y < 0)
-    {
-      ny = p.parameters.h.value + y;
-    }
-    else if (y >= p.parameters.h.value)
-    {
-      ny = y - p.parameters.h.value;
-    }
-    else
-    {
-      ny = y;
-    }
-    return grid[nx][ny];
-    
-    
-    if (x < 0) {
-      nx = x-(x%p.parameters.w.value)
-    }
-    else
-    {
-      nx = x%p.parameters.w.value;
-    }
-    if (y < 0) {
-      ny = y-(y%p.parameters.h.value)
-    }
-    else
-    {
-      ny = y%p.parameters.h.value;
-    }
-    return grid[nx][ny];
-  }
-  
-  
-  function laplaceA(x, y) {
-    var sumA = 0;
-    var rate = 0.146;
-    sumA += grid[x][y].a * -1;
-    sumA += getxy(x - 1, y).a * rate;
-    sumA += getxy(x + 1, y).a * rate;
-    sumA += getxy(x, y + 1).a * rate;
-    sumA += getxy(x, y - 1).a * rate;
-    sumA += getxy(x - 1, y - 1).a * rate*diagonal_rate;
-    sumA += getxy(x + 1, y - 1).a * rate*diagonal_rate;
-    sumA += getxy(x + 1, y + 1).a * rate*diagonal_rate;
-    sumA += getxy(x - 1, y + 1).a * rate*diagonal_rate;
-    return sumA;
-  }
-  
-  function laplaceB(x, y) {
-    var sumB = 0;
-    var rate = 0.146;
-    sumB += grid[x][y].b * -1;
-    sumB += getxy(x - 1, y).b * rate;
-    sumB += getxy(x + 1, y).b * rate;
-    sumB += getxy(x, y + 1).b * rate;
-    sumB += getxy(x, y - 1).b * rate;
-    sumB += getxy(x - 1, y - 1).b * rate*diagonal_rate;
-    sumB += getxy(x + 1, y - 1).b * rate*diagonal_rate;
-    sumB += getxy(x + 1, y + 1).b * rate*diagonal_rate;
-    sumB += getxy(x - 1, y + 1).b * rate*diagonal_rate;
-    return sumB;
   }
 
-  function swap() {
-      var temp = grid;
-      grid = next;
-      next = temp;
-  }
-
-  // Method to restart the simulation
-  p.restart = function() {
-      initializeGrid();
-  };
-};
-
-new p5(sketch3, 'container2');
+  function setPixelColor(pixelIndex, colorVal) {
+      p.pixels[pixelIndex + 0] = colorVal;
+      p.pixels[pixelIndex + 1]
