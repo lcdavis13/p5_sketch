01d43807a5d0642ca3be42ef55ff9584a1993764
lcdavis13 <3443196+lcdavis13@users.noreply.github.com>
2024-02-05 11:10:42 -1000

    destructuring parameters

--- sketch3.js
+++ sketch3.js
@@ -13,41 +13,52 @@ var sketch3 = function(p) {
     dt: { label: 'Time Delta', min: 0.8, max: 1.2, step: 0.01, value: 1.0 }
   };
 
+  // Helper function to destructure parameters
+  function params() {
+    return Object.keys(p.parameters).reduce((values, key) => {
+      values[key] = p.parameters[key].value;
+      return values;
+    }, {});
+  }
+
   p.setup = function() {
-      p.createCanvas(200, 200);
-      p.pixelDensity(1);
-      initializeGrid();
+    const { w, h } = params();
+    p.createCanvas(w, h);
+    p.pixelDensity(1);
+    initializeGrid();
   };
 
   function initializeGrid() {
+    const { w, h } = params();
     grid = [];
     next = [];
-    for (var x = 0; x < p.parameters.w.value; x++) {
-        grid[x] = [];
-        next[x] = [];
-        for (var y = 0; y < p.parameters.h.value; y++) {
-            grid[x][y] = { a: 1, b: 0 };
-            next[x][y] = { a: 1, b: 0 };
-        }
+    for (var x = 0; x < w; x++) {
+      grid[x] = [];
+      next[x] = [];
+      for (var y = 0; y < h; y++) {
+        grid[x][y] = { a: 1, b: 0 };
+        next[x][y] = { a: 1, b: 0 };
+      }
     }
 
     for (var i = 150; i < 160; i++) {
-        for (var j = 150; j < 160; j++) {
-            grid[i][j].b = 1.0; //0.5 + 0.5 * p.random();
-        }
+      for (var j = 150; j < 160; j++) {
+        grid[i][j].b = 1.0; // Seed some initial value for B
+      }
     }
   }
 
   p.draw = function() {
-      p.background(51);
-      updateGrid();
-      drawGrid();
-      swap();
+    p.background(51);
+    updateGrid();
+    drawGrid();
+    swap();
   };
 
   function updateGrid() {
-    for (var x = 0; x < p.parameters.w.value; x++) {
-      for (var y = 0; y < p.parameters.h.value; y++) {
+    const { w, h } = params();
+    for (var x = 0; x < w; x++) {
+      for (var y = 0; y < h; y++) {
         var laplaceAVal = laplace(x, y, 'a');
         var laplaceBVal = laplace(x, y, 'b');
         var a = grid[x][y].a;
@@ -60,63 +71,67 @@ var sketch3 = function(p) {
   }
 
   function updateConcentrationA(a, b, laplaceAVal) {
-      var reactionTermA = -a * b * b + p.parameters.feed.value * (1 - a);
-      var diffusionTermA = p.parameters.dA.value * laplaceAVal;
-      var updatedA = a + p.parameters.dt.value * (diffusionTermA + reactionTermA);
-      return p.constrain(updatedA, 0, 1);
+    const { dA, feed, dt } = params();
+    var reactionTermA = -a * b * b + feed * (1 - a);
+    var diffusionTermA = dA * laplaceAVal;
+    var updatedA = a + dt * (diffusionTermA + reactionTermA);
+    return p.constrain(updatedA, 0, 1);
   }
 
   function updateConcentrationB(b, a, laplaceBVal) {
-      var reactionTermB = a * b * b - (p.parameters.k.value + p.parameters.feed.value) * b;
-      var diffusionTermB = p.parameters.dB.value * laplaceBVal;
-      var updatedB = b + p.parameters.dt.value * (diffusionTermB + reactionTermB);
-      return p.constrain(updatedB, 0, 1);
+    const { dB, k, feed, dt } = params();
+    var reactionTermB = a * b * b - (k + feed) * b;
+    var diffusionTermB = dB * laplaceBVal;
+    var updatedB = b + dt * (diffusionTermB + reactionTermB);
+    return p.constrain(updatedB, 0, 1);
   }
-
 
   function laplace(x, y, type) {
-      var sum = 0;
-      var rate = 1.0/(4.0*diagonal_rate + 4.0); // sums to 1 for all incoming cells
+    const { w, h } = params();
+    var sum = 0;
+    var rate = 1.0 / (4.0 * diagonal_rate + 4.0); // Adjust to sum to 1 for all incoming cells
 
-      sum += grid[x][y][type] * -1;
-      sum += getNeighborConcentration(x - 1, y, type) * rate;
-      sum += getNeighborConcentration(x + 1, y, type) * rate;
-      sum += getNeighborConcentration(x, y - 1, type) * rate;
-      sum += getNeighborConcentration(x, y + 1, type) * rate;
-      sum += getNeighborConcentration(x - 1, y - 1, type) * rate * diagonal_rate;
-      sum += getNeighborConcentration(x + 1, y - 1, type) * rate * diagonal_rate;
-      sum += getNeighborConcentration(x + 1, y + 1, type) * rate * diagonal_rate;
-      sum += getNeighborConcentration(x - 1, y + 1, type) * rate * diagonal_rate;
+    sum += grid[x][y][type] * -1;
+    sum += getNeighborConcentration(x - 1, y, type) * rate;
+    sum += getNeighborConcentration(x + 1, y, type) * rate;
+    sum += getNeighborConcentration(x, y - 1, type) * rate;
+    sum += getNeighborConcentration(x, y + 1, type) * rate;
+    sum += getNeighborConcentration(x - 1, y - 1, type) * rate * diagonal_rate;
+    sum += getNeighborConcentration(x + 1, y - 1, type) * rate * diagonal_rate;
+    sum += getNeighborConcentration(x + 1, y + 1, type) * rate * diagonal_rate;
+    sum += getNeighborConcentration(x - 1, y + 1, type) * rate * diagonal_rate;
 
-      return sum;
-  }
+    return sum;
+}
 
-  function getNeighborConcentration(x, y, type) {
-      var nx = (x + p.parameters.w.value) % p.parameters.w.value;
-      var ny = (y + p.parameters.h.value) % p.parameters.h.value;
-      return grid[nx][ny][type];
-  }
+function getNeighborConcentration(x, y, type) {
+    const { w, h } = params();
+    var nx = (x + w) % w;
+    var ny = (y + h) % h;
+    return grid[nx][ny][type];
+}
 
-  function drawGrid() {
-      p.loadPixels();
-      for (var x = 0; x < p.parameters.w.value; x++) {
-          for (var y = 0; y < p.parameters.h.value; y++) {
-              var pix = (x + y * p.parameters.w.value) * 4;
-              var a = next[x][y].a;
-              var b = next[x][y].b;
-              var colorVal = p.floor((a - b) * 255);
-              colorVal = p.constrain(colorVal, 0, 255);
-              setPixelColor(pix, colorVal);
-          }
-      }
-      p.updatePixels();
-  }
+function drawGrid() {
+    const { w, h } = params();
+    p.loadPixels();
+    for (var x = 0; x < w; x++) {
+        for (var y = 0; y < h; y++) {
+            var pix = (x + y * w) * 4;
+            var a = next[x][y].a;
+            var b = next[x][y].b;
+            var colorVal = p.floor((a - b) * 255);
+            colorVal = p.constrain(colorVal, 0, 255);
+            setPixelColor(pix, colorVal);
+        }
+    }
+    p.updatePixels();
+}
 
-  function setPixelColor(pixelIndex, colorVal) {
+function setPixelColor(pixelIndex, colorVal) {
     p.pixels[pixelIndex + 0] = colorVal; // Red
     p.pixels[pixelIndex + 1] = colorVal; // Green
     p.pixels[pixelIndex + 2] = colorVal; // Blue
-    p.pixels[pixelIndex + 3] = 255;       // Alpha
+    p.pixels[pixelIndex + 3] = 255; // Alpha
 }
 
 function swap() {
